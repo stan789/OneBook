24d09204b258a11ea1c20d89dfdd2828182a6d83
Henry Ang <FrozVentus@users.noreply.github.com>
2017-10-18 20:58:27 +0800

    [Ui change] Add Event for showing filtered list

---
+++ src/main/java/seedu/address/commons/events/commands/DisplayListFilteredEvent.java
@@ -0,0 +1,15 @@
+package seedu.address.commons.events.commands;
+
+import seedu.address.commons.events.BaseEvent;
+
+/** Indicates the AddressBook in the display list is now filtered*/
+public class DisplayListFilteredEvent extends BaseEvent {
+
+    public DisplayListFilteredEvent() {
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+}
