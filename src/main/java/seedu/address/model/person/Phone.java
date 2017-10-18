8b493b1ed94d60660bef22a97be97c913fa0092e
stan <e0031265@u.nus.edu>
2017-10-18 10:39:07 +0800

    modify regex in Name and Phone to accept + and * for phone and some special char for name

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -12,8 +12,9 @@ public class Phone {
 
 
     public static final String MESSAGE_PHONE_CONSTRAINTS =
-            "Phone numbers can only contain numbers, and should be at least 3 digits long";
-    public static final String PHONE_VALIDATION_REGEX = "\\d{3,}";
+            "Phone numbers can only contain numbers and + or *, and should be at least 3 digits long";
+    public static final String PHONE_VALIDATION_REGEX = "[0-9.+*]\\d{3,}";
+    public static final String PHONE_VALIDATION_REGEX1 = "\\d{3,}";
     public final String value;
 
     /**
@@ -34,7 +35,7 @@ public class Phone {
      * Returns true if a given string is a valid person phone number.
      */
     public static boolean isValidPhone(String test) {
-        return test.matches(PHONE_VALIDATION_REGEX);
+        return (test.matches(PHONE_VALIDATION_REGEX) || test.matches(PHONE_VALIDATION_REGEX1));
     }
 
     @Override
