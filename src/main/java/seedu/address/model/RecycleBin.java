afc05dcafbe475c0f727c93a2cb73bfd66db0350
Henry Ang <FrozVentus@users.noreply.github.com>
2017-10-21 16:53:58 +0800

    [RecycleBin] Create RecycleBin class

---
+++ src/main/java/seedu/address/model/RecycleBin.java
@@ -0,0 +1,10 @@
+package seedu.address.model;
+
+
+/**
+ * An extension of AddressBook to hold deleted contacts
+ * Duplicates are not allowed (by .equals comparison)
+ */
+public class RecycleBin extends AddressBook {
+
+}
