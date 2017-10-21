c361e5cb9f2d283faf31d61a0ffc91a74a2b0308
Henry Ang <FrozVentus@users.noreply.github.com>
2017-10-21 20:11:55 +0800

    [RecycleBin] Add AddressBookData to hold addressbook and recyclebin data

---
+++ src/main/java/seedu/address/storage/AddressBookData.java
@@ -0,0 +1,46 @@
+package seedu.address.storage;
+
+import seedu.address.model.AddressBook;
+import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.RecycleBin;
+
+/**
+ * A storage data holder to hold both addressBook and recycleBin data
+ */
+public class AddressBookData {
+
+    private ReadOnlyAddressBook addressBook;
+    private ReadOnlyAddressBook recycleBin;
+
+    public AddressBookData() {
+        addressBook = new AddressBook();
+        recycleBin = new RecycleBin();
+    }
+
+    public AddressBookData(ReadOnlyAddressBook addressBook) {
+        this();
+        this.addressBook = addressBook;
+    }
+
+    public AddressBookData(ReadOnlyAddressBook addressBook, ReadOnlyAddressBook recycleBin) {
+        this();
+        this.addressBook = addressBook;
+        this.recycleBin = recycleBin;
+    }
+
+    public void setAddressBook (ReadOnlyAddressBook addressBook) {
+        this.addressBook = addressBook;
+    }
+
+    public void setRecycleBin (ReadOnlyAddressBook recycleBin) {
+        this.recycleBin = recycleBin;
+    }
+
+    public ReadOnlyAddressBook getAddressBook () {
+        return addressBook;
+    }
+
+    public ReadOnlyAddressBook getRecycleBin () {
+        return recycleBin;
+    }
+}
