8ea0a87e78d3f4715ee05734e3b7f778864e400b
stan789 <e0031265@u.nus.edu>
2017-10-26 16:05:34 +0800

    remove allow command case insensitive

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -50,7 +50,7 @@ public class AddressBookParser {
 
         final String commandWord = matcher.group("commandWord");
         final String arguments = matcher.group("arguments");
-        switch (commandWord.toLowerCase()) {
+        switch (commandWord) {
 
         case AddCommand.COMMAND_WORD:
             return new AddCommandParser().parse(arguments);
