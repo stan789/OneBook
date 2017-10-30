25513112f1f9ce62079302d3ef1d40c098424ed3
darrinloh <e0031557@u.nus.edu>
2017-10-30 23:23:05 +0800

    wrote some exceptions to handle backup file exceptions

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -103,8 +103,19 @@ public class MainApp extends Application {
             logger.warning("Data file not in the correct format. Will be starting with an empty AddressBook");
             initialData = new AddressBookData();
         } catch (IOException e) {
-            logger.warning("Problem while reading from the file. Will be starting with an empty AddressBook");
-            initialData = new AddressBookData();
+            logger.warning("Problem while reading from the file. Will start backup file");
+            try{
+                addressBookOptional = storage.readBackUpAddressBook();
+                initialData = addressBookOptional.get();
+            }
+            catch (DataConversionException f) {
+                logger.warning("Backup file is not in the correct format. Will be starting with an empty AddressBook");
+                initialData = new AddressBookData();
+            }
+            catch (IOException f) {
+                logger.warning("Problem while reading from the backup file. Will be starting with an empty AddressBook");
+                initialData = new AddressBookData();
+            }
         }
 
         return new ModelManager(initialData.getAddressBook(), initialData.getRecycleBin(), userPrefs);
