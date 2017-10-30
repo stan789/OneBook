f238dc4bc26b683692e31c4efbfb69e4b856a9cf
GIDEON-PC\Gideon Fu <gideonfu@outlook.com>
2017-10-30 16:06:28 +0800

    [Remark] Fix checkstyle errors

--- src/test/java/systemtests/AddCommandSystemTest.java
+++ src/test/java/systemtests/AddCommandSystemTest.java
@@ -39,7 +39,6 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_ORGANISATION_BO
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARK_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARK_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.testutil.TypicalPersons.ALICE;
@@ -121,7 +120,8 @@ public class AddCommandSystemTest extends AddressBookSystemTest {
         /* Case: add a person with all fields same as another person in the address book except name -> added */
         toAdd = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_AMY)
                 .withBirthday(VALID_BIRTHDAY_AMY).withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY)
-                .withOrganisation(VALID_ORGANISATION_AMY).withRemark(VALID_REMARK_AMY).withTags(VALID_TAG_FRIEND).build();
+                .withOrganisation(VALID_ORGANISATION_AMY).withRemark(VALID_REMARK_AMY)
+                .withTags(VALID_TAG_FRIEND).build();
         command = AddCommand.COMMAND_WORD + NAME_DESC_BOB + PHONE_DESC_AMY + BIRTHDAY_DESC_AMY
                 + EMAIL_DESC_AMY + ADDRESS_DESC_AMY + ORGANISATION_DESC_AMY + REMARK_DESC_AMY + TAG_DESC_FRIEND;
         assertCommandSuccess(command, toAdd);
@@ -129,7 +129,8 @@ public class AddCommandSystemTest extends AddressBookSystemTest {
         /* Case: add a person with all fields same as another person in the address book except phone -> added */
         toAdd = new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_BOB)
                 .withBirthday(VALID_BIRTHDAY_AMY).withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY)
-                .withOrganisation(VALID_ORGANISATION_AMY).withRemark(VALID_REMARK_AMY).withTags(VALID_TAG_FRIEND).build();
+                .withOrganisation(VALID_ORGANISATION_AMY).withRemark(VALID_REMARK_AMY)
+                .withTags(VALID_TAG_FRIEND).build();
         command = AddCommand.COMMAND_WORD + NAME_DESC_AMY + PHONE_DESC_BOB + BIRTHDAY_DESC_AMY
                 + EMAIL_DESC_AMY + ADDRESS_DESC_AMY + ORGANISATION_DESC_AMY + REMARK_DESC_AMY + TAG_DESC_FRIEND;
         assertCommandSuccess(command, toAdd);
@@ -137,7 +138,8 @@ public class AddCommandSystemTest extends AddressBookSystemTest {
         /* Case: add a person with all fields same as another person in the address book except birthday -> added */
         toAdd = new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY)
                 .withBirthday(VALID_BIRTHDAY_BOB).withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY)
-                .withOrganisation(VALID_ORGANISATION_AMY).withRemark(VALID_REMARK_AMY).withTags(VALID_TAG_FRIEND).build();
+                .withOrganisation(VALID_ORGANISATION_AMY).withRemark(VALID_REMARK_AMY)
+                .withTags(VALID_TAG_FRIEND).build();
         command = AddCommand.COMMAND_WORD + NAME_DESC_AMY + PHONE_DESC_AMY + BIRTHDAY_DESC_BOB
                 + EMAIL_DESC_AMY + ADDRESS_DESC_AMY + ORGANISATION_DESC_AMY + REMARK_DESC_AMY + TAG_DESC_FRIEND;
         assertCommandSuccess(command, toAdd);
