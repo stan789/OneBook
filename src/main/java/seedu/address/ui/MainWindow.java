c7f7d1595367bd685abf87aa65717f12d266513e
darrinloh <e0031557@u.nus.edu>
2017-11-08 21:29:19 +0800

    bug fix

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -37,6 +37,7 @@ public class MainWindow extends UiPart<Region> {
     private static final String FXML = "MainWindow.fxml";
     private static final int MIN_HEIGHT = 600;
     private static final int MIN_WIDTH = 450;
+    private static AddressPanel addressPanel;
     private static Scene scene;
     private final Logger logger = LogsCenter.getLogger(this.getClass());
 
@@ -44,7 +45,6 @@ public class MainWindow extends UiPart<Region> {
     private Logic logic;
 
     // Independent Ui parts residing in this Ui container
-    private static AddressPanel addressPanel;
     private PersonListPanel personListPanel;
     private Config config;
     private UserPrefs prefs;
