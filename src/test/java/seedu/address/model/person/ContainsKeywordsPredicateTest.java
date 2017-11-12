24596693ce04dbd4a07e2f70f483f6cd94605b88
GIDEON-PC\Gideon Fu <gideonfu@outlook.com>
2017-11-12 20:16:08 +0800

    Fix checkstyle error

--- src/test/java/seedu/address/model/person/ContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/person/ContainsKeywordsPredicateTest.java
@@ -3,7 +3,6 @@ package seedu.address.model.person;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
