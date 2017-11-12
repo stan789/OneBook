b574cdcd69b3912cb07c9ff906b56f0081e5daa3
darrinloh <e0031557@u.nus.edu>
2017-11-13 03:29:15 +0800

    added 2 more tests

--- src/test/java/seedu/address/ui/ModeChangeTest.java
+++ src/test/java/seedu/address/ui/ModeChangeTest.java
@@ -2,6 +2,7 @@ package seedu.address.ui;
 
 import static org.junit.Assert.assertTrue;
 import static seedu.address.ui.MainWindow.DARK_MODE;
+import static seedu.address.ui.MainWindow.LIGHT_MODE;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -23,5 +24,17 @@ public class ModeChangeTest {
     @Test
     public void check_valid_css() {
         assertTrue(prefs.getTheme().contains(DARK_MODE));
+    }
+
+    @Test
+    public void check_valid_css_after_updating() {
+        prefs.updateLastUsedThemeSetting(DARK_MODE);
+        assertTrue(prefs.getTheme().contains(DARK_MODE));
+    }
+
+    @Test
+    public void set_css() {
+        prefs.setTheme(LIGHT_MODE);
+        assertTrue(prefs.getTheme().contains(LIGHT_MODE));
     }
 }
